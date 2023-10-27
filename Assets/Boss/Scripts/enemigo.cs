46a4dfc1146f3e6b39edd3e08c2482ad3a9f7618
Ingrid10b <ingridbenzi331@gmail.com>
2023-10-27 02:43:03 -0300

    ataques

---
+++ Assets/Boss/Scripts/enemigo.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+public class enemigo : MonoBehaviour
+{
+    public int damage = 10;
+    public float attackCooldown = 2.0f;
+    private float lastAttackTime = 0.0f;
+    private Animator animator;
+
+
+    private Transform player; // Referencia al objeto del jugador.
+    private NavMeshAgent navMeshAgent;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+        player = GameObject.FindWithTag("Player").transform; // Asigna el jugador por etiqueta "Player".
+        navMeshAgent = GetComponent<NavMeshAgent>();
+
+
+
+    }
+    private void Update()
+    {
+        if (player != null)
+        {
+            // Sigue al jugador.
+            navMeshAgent.SetDestination(player.position);
+
+            if (Vector3.Distance(transform.position, player.position) <= navMeshAgent.stoppingDistance)
+            {
+                // El enemigo ha alcanzado al jugador, atacar.
+                Attack();
+            }
+        }
+    }
+
+    private void Attack()
+    {
+        if (Time.time - lastAttackTime >= attackCooldown)
+        {
+            // Realiza la lógica de ataque al jugador aquí.
+            Vida playerHealth = player.GetComponent<Vida>();
+            if (playerHealth != null)
+            {
+
+                playerHealth.TakeDamage(damage);
+                animator.SetTrigger("atacar");
+                lastAttackTime = Time.time;
+            }
+        }
+    }
+}
---
+++ Assets/EnemigoAI.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemigoAI : MonoBehaviour
+{
+    public int rutina;
+    public float cronometro;
+    public Animator ani;
+    public Quaternion anguloGiro;
+    public float grado;
+
+
+    void Start()
+    {
+        ani = GetComponent<Animator>();
+        //navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    public void ComportamientoEnemigo()
+    {
+        cronometro += 1 * Time.deltaTime;
+        if (cronometro >= 4)
+        {
+            rutina = Random.Range(0, 2);
+            cronometro = 0;
+        }
+        switch (rutina)
+        {
+            case 0:
+                //el enemigo se mantendrá quieto
+                ani.SetBool("Walk", false);
+                break;
+            case 1:
+                //determinamos la dirección en la que desplaza el enemigo
+                grado = Random.Range(0, 360);
+                anguloGiro = Quaternion.Euler(0, grado, 0);
+                rutina++;
+                break;
+            case 2:
+                //determinamos el movimiento del enemigo
+                //la rotacion del enemigo es = al angulo establecido anteriormente
+                transform.rotation = Quaternion.RotateTowards(transform.rotation, anguloGiro, 0.5f);
+                //se mueve hacia adelante con una velocidad de 1
+                transform.Translate(Vector3.forward * 1 * Time.deltaTime);
+                //activamos la animacion de caminar
+                ani.SetBool("Walk", true);
+                break;
+        }
+    }
+
+    void Update()
+    {
+        ComportamientoEnemigo();
+        //navMeshAgent.SetDestination(target.position);
+    }
+}
