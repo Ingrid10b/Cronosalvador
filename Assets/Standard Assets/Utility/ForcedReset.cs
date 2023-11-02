6453fc5d7aa88dd8ffd3d39b0e4803306f75f059
PLuciaPereira <lucia@epereira.com.ar>
2023-11-02 17:10:44 -0300

    cambios lu

--- Assets/Standard Assets/Utility/ForcedReset.cs
+++ Assets/Standard Assets/Utility/ForcedReset.cs
@@ -11,7 +11,7 @@ public class ForcedReset : MonoBehaviour
     private void Update()
     {
         // if we have forced a reset ...
-        if (CrossPlatformInputManager.GetButtonDown("ResetObject"))
+        //if (CrossPlatformInputManager.GetButtonDown("ResetObject"))
         {
             //... reload the scene
             SceneManager.LoadScene(SceneManager.GetSceneAt(0).name);
